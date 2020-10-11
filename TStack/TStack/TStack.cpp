b4d137ad110f700f4cc309311743e998f48116ef
Илья Колесников <58778310+ShaldonS@users.noreply.github.com>
2020-10-11 12:45:27 +0300

    Add files via upload
    Проект на начальном этапе содержит стек, работающий с массивом чисел; главный исполнительный файл, содержащий блок, выполняющий проверку скобок, замену стандартной инфиксной формы арифметического выражения на постфиксную.

---
+++ TStack/TStack/TStack.cpp
@@ -0,0 +1,92 @@
+﻿#include <algorithm>
+#include <stack>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	stack <char> steck;
+	string str = "2 * 4 + 3";
+
+
+	cout << str;
+
+	string out = "";
+
+	//оформить функцией
+	for (int i = 0; i < str.length(); i++)
+	{
+		if (str[i] == ' ') continue;
+		switch (str[i])
+		{
+		case '(': steck.push('('); break;
+		case ')':
+
+			while (!steck.empty() || steck.top() != '(')
+			{
+				out.push_back(steck.top());
+				steck.pop();
+			}
+			steck.pop();
+			break;
+
+		case '+':
+			if (steck.empty())
+				steck.push('+');
+			else
+			{
+				while (!steck.empty())
+				{
+					if (steck.top() != '(')
+					{
+						out.push_back(steck.top());
+						steck.pop();
+					}
+					else break;
+				}
+				steck.push('+');
+			}
+			break;
+		case '-':
+			if (steck.empty())
+				steck.push('+');
+			else
+			{
+				while (!steck.empty() || steck.top() != '(')
+				{
+					out.push_back(steck.top());
+					steck.pop();
+				}
+				steck.push('+');
+			}
+			break;
+		case '*':
+		case '/':
+			if (steck.empty())
+				steck.push(str[i]);
+			else
+			{
+				while (!steck.empty() || steck.top() == '*' || steck.top() == '/')
+				{
+					out.push_back(steck.top());
+					steck.pop();
+				}
+				steck.push(str[i]);
+			}
+			break;
+		default: out.push_back(str[i]);
+		}
+	}
+
+	while (!steck.empty())
+	{
+		out.push_back(steck.top());
+		steck.pop();
+	}
+
+	cout << endl << out;
+
+	return 0;
+}
